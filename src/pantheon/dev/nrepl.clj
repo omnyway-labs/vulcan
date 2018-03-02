836f58300d5df9d711093d0f66ba001acd816dc2
icylisper <icylisper@gmail.com>
2018-03-02 08:29:44 -0800

    first commit

---
+++ src/pantheon/dev/nrepl.clj
@@ -0,0 +1,14 @@
+(ns pantheon.dev.nrepl
+  (:require
+   [clojure.tools.nrepl.server :refer [start-server]]
+   [cider.nrepl :refer [cider-nrepl-handler]]))
+
+(defn start []
+  (let [port (or (some-> (first *command-line-args*)
+                       (java.lang.Long/parseLong))
+                 9000)]
+    (start-server :port port :handler cider-nrepl-handler)
+    (println "Started nREPL on port" port)))
+
+(defn -main [& args]
+  (start))
