391d1a77cee42e65e316bb0870896b2401ec89ac
icylisper <icylisper@gmail.com>
2018-03-03 10:52:37 -0800

    serialize dependents as sorted-set

--- src/pantheon/tools/deps.clj
+++ src/pantheon/tools/deps.clj
@@ -65,7 +65,7 @@
   (-> dep-map
       (select-keys [:mvn/version :git/url :local/root
                     :sha :exclusions :dependents])
-      (update-in [:dependents] (comp distinct sort))
+      (update-in [:dependents] (partial apply sorted-set))
       (u/remove-nil-entries)))
 
 (defn flatten-deps [deps]
